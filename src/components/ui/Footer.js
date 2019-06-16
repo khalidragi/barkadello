2e9fc28c16a230f7a6204074e7c8475c34b3b4b8
Khalid Ragi <khalid.ragi@gmail.com>
2019-06-16 18:52:15 +0000

    added a footer

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './footer.css'
+import { Icon } from 'semantic-ui-react';
+
+const Footer = () => (
+  <footer className='footer'>
+            <Icon className='footer-icons' link name='snapchat' size='big' />
+            <Icon className='footer-icons' link name='whatsapp 'size='big' />
+            <Icon className='footer-icons' link name='instagram 'size='big' />
+            <Icon className='footer-icons' link name='facebook square 'size='big' />
+            <p>    Made with ❤ by Khalid Ragi, © 2019 </p>
+  </footer>
+);
+
+export default Footer;
