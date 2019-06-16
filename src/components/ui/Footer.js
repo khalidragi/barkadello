6c2069a01c8fc967de37befdf28e1c991593593e
Khalid Ragi <khalid.ragi@gmail.com>
2019-06-16 20:22:16 +0000

    footer

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,19 +4,11 @@ import { Icon } from 'semantic-ui-react';
 
 const Footer = () => (
   <footer className='footer'>
-<<<<<<< HEAD
-            <Icon className='footer-icons' link name='snapchat' size='big'><i href='https://www.facebook.com/Barkadello-415554725463540/?ref=br_rs'></i> </Icon>
-            <Icon className='footer-icons' link name='whatsapp 'size='big' />
-            <Icon className='footer-icons' link name='instagram 'size='big' />
-            <Icon className='footer-icons' link name='facebook square 'size='big' />
-            <p>Made with ❤ by Khalid Ragi, © 2019 </p>
-=======
             <Icon className='footer-icons' link name='snapchat' size='big' />
             <Icon className='footer-icons' link name='whatsapp 'size='big' />
             <Icon className='footer-icons' link name='instagram 'size='big' />
             <Icon className='footer-icons' link name='facebook square 'size='big' />
             <p>    Made with ❤ by Khalid Ragi, © 2019 </p>
->>>>>>> origin/master
   </footer>
 );
 
